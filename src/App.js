b1c6c8ccc06d935b3f4bafc5efedfe9dd5ec3c59
Savan Patel <dankharasavan9624@gmail.com>
2021-11-21 01:34:28 +0530

    init alexa

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, useLocation } from "react-router-dom";
+// import Footer from "./components/Footer";
+// import Menubar from "./components/Menubar";
+// import HomeRouter from "./routes";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./sass/style.scss";
+import "./index.css";
+import WrapperLayout from "./WrapperLayout";
+import HomeRouter from "./routes";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
+
+function App() {
+  return (
+    <Router>
+      <ScrollToTop />
+      <HomeRouter />
+    </Router>
+  );
+}
+
+export default App;
