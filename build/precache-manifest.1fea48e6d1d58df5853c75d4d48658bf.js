30ca77eedfdc12749ca01ec3508606f8ae4157c0
Savan Patel <dankharasavan9624@gmail.com>
2021-11-25 20:53:43 +0530

    update Api url

---
+++ build/precache-manifest.1fea48e6d1d58df5853c75d4d48658bf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d1c8f89a31cf761ada2cfe88cccaa88",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8b28cdfe1939b5b31ad",
+    "url": "/static/css/2.eee77869.chunk.css"
+  },
+  {
+    "revision": "3de97ea40aa88a6ffe64",
+    "url": "/static/css/main.e9802888.chunk.css"
+  },
+  {
+    "revision": "c8b28cdfe1939b5b31ad",
+    "url": "/static/js/2.5cfd10dd.chunk.js"
+  },
+  {
+    "revision": "6cc66abdd9fdddb3ca522b51ab7836c6",
+    "url": "/static/js/2.5cfd10dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3de97ea40aa88a6ffe64",
+    "url": "/static/js/main.c70588d2.chunk.js"
+  },
+  {
+    "revision": "29f877d757429398a816",
+    "url": "/static/js/runtime-main.6685fa00.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "57ccbd9cdda41c62f3f670c448e9bcee",
+    "url": "/static/media/comNavigation.57ccbd9c.jpg"
+  },
+  {
+    "revision": "598af92182f7fc6d332b0a5384e12664",
+    "url": "/static/media/productionBg.598af921.jpg"
+  }
+]);
